15c21b7930da42345ef78cba3d0bd47f31fb1457
meilizheng <116130097+meilizheng@users.noreply.github.com>
2025-05-20 20:49:39 -0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import RestaurantsContainer from "./components/RestaurantsContainer";
+import { data } from "./data/data.js"; // <-- Make sure this import is here
 import "./App.css";
 
-//App is the root of our application and where we load in our components.
 function App() {
-
-
   return (
     <div className="App">
+      <RestaurantsContainer restaurants={data} />
     </div>
   );
 }
--- src/components/RestaurantsContainer.jsx
+++ src/components/RestaurantsContainer.jsx
@@ -1,12 +1,14 @@
-
+import { data } from "../data/data.js";
 import Restaurant from "./Restaurant";
 
-//Containers are for behind the scenes logic  
+// Containers are for behind-the-scenes logic  
 function RestaurantsContainer(props) {
-  console.log(props)
+  console.log(props);
   return (
     <div className="restaurantContainer">
-      {props.restaurants.map(restaurant => <Restaurant restaurant={restaurant} />)}
+      {props.restaurants.map((restaurant) => (
+        <Restaurant key={restaurant.id} restaurant={restaurant} />
+      ))}
     </div>
   );
 }
